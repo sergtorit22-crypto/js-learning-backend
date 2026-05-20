287fbe130840b77f0bd17567805ca1f53539938a
Serg Toropov <s.toropov@student.sumdu.edu.ua>
2026-05-20 16:35:13 +0300

    Оновив дизайн уроків

--- server/db.js
+++ server/db.js
@@ -1,25 +1,19 @@
-/*const { Pool } = require("pg");
-
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "lessonsdb",
-  password: "sergtorit-22",
-  port: 5432,
-});
-
-module.exports = pool;*/
 const { Pool } = require("pg");
 
-// Вставь СВОЮ строку из Neon вместо этого примера:
 const connectionString =
   "postgresql://neondb_owner:npg_lYg4WyXnwve8@ep-lingering-tree-almuarr5.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require";
 
 const pool = new Pool({
   connectionString: connectionString,
   ssl: {
-    rejectUnauthorized: false, // ОБЯЗАТЕЛЬНО для работы SSL в облаке Neon
+    rejectUnauthorized: false,
   },
 });
 
 module.exports = pool;
+
+/*
+git add .
+git commit -m "Оновив дизайн уроків"
+git push origin master
+*/
